85bf2e6f2991442d67ce5e23093ac77ffc972e20
nlekkerman <nlekkerman@gmail.com>
2023-12-23 03:27:57 +0000

    pushed

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.get('/', (req, res) => {
 
+  res.send('Hello What is this')
+})
 
 
 app.get('/players', async(req, res) => {
